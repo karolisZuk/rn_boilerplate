8b331708005f48d5fed467928bcc4a95a467b655
Karolis <kar.zukas@gmail.com>
2019-11-17 13:01:12 +0200

    finished

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View>
-      <Text style={{ fontSize: 30 }}>HomeScreen</Text>
-      <Button title="Layout" onPress={() => navigation.navigate('Layout')} />
-    </View>
-  );
+const HomeScreen = (props) => {
+    const { navigate } = props.navigation;
+
+    return (
+        <View>
+            <Text style={styles.text}>HomeScreen</Text>
+            <Button
+                title="Go to Components Demo"
+                onPress={() => navigate('Components')}
+            />
+            <Button
+                title="Go to Image Demo"
+                onPress={() => navigate('Image')}
+            />
+            <Button
+                title="Go to List Demo"
+                onPress={() => navigate('List')}
+            />
+        </View>
+    );
 };
 
-HomeScreen.navigationOptions = {
-  title: 'Home'
-};
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 30
+    }
+});
 
 export default HomeScreen;
