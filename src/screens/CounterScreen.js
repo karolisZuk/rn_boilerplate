e10777f79b9bfaa8460713420f5236b92d057380
Karolis <kar.zukas@gmail.com>
2019-12-02 16:31:31 +0200

    Kodas nenaudojant state, todel vaizdas neatsinaujina, nors skaicius tikrai updatinasi

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -3,8 +3,18 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
 
+    let counter = 0;
+
     return (
         <View>
+            <Button title="Increase" onPress={ () => {
+                counter++;
+                console.log(counter);
+            }} />
+            <Button title="Decrease" onPress={ () => {
+                counter--;
+                console.log(counter);
+            } } />
             <Text>Current Count: {counter}</Text>
         </View>
     );
